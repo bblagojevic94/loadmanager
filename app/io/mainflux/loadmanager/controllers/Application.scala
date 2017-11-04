ef66439df222fd9c017289867b45ca289cc09a1b
Bojan Blagojevic <datiglavaradi@gmail.com>
2017-11-04 23:16:27 +0100

    Document the API using swagger
    API is documented using openapi 3.0 standard.

--- app/io/mainflux/loadmanager/controllers/Application.scala
+++ app/io/mainflux/loadmanager/controllers/Application.scala
@@ -6,4 +6,14 @@ class Application extends Controller {
 
   def health = Action { Ok("") }
 
+  def docs = Action {
+    import Application._
+    Redirect(url = SwaggerUrl, queryString = Map(UrlQuery -> Seq(OpenAPIUrl)))
+  }
+}
+
+object Application {
+  val SwaggerUrl = "/assets/lib/swagger-ui/index.html"
+  val OpenAPIUrl = "/assets/openapi.yaml"
+  val UrlQuery   = "url"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,6 +2,4 @@ addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.18")
 
 addSbtPlugin("com.geirsson" % "sbt-scalafmt" % "0.6.8")
 
-addSbtPlugin("com.iheart" % "sbt-play-swagger" % "0.5.3")
-
 addSbtPlugin("au.com.onegeek" %% "sbt-dotenv" % "1.1.36")
