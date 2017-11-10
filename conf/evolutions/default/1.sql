419c48abc7c79b28ef965d4b4bed07791e743c8b
Bojan Blagojevic <datiglavaradi@gmail.com>
2017-11-11 00:47:49 +0100

    Add evolution script
    Create sql script for creating microgrids and groups.

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- !Ups
+
+CREATE TABLE microgrids (
+  id                BIGSERIAL PRIMARY KEY,
+  url               TEXT NOT NULL,
+  platform          VARCHAR(10) NOT NULL,
+  organisation_id   VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE groups (
+  id     BIGSERIAL PRIMARY KEY,
+  name   VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE groups_microgrids (
+  group_id       BIGSERIAL NOT NULL,
+  microgrid_id   BIGSERIAL NOT NULL,
+  CONSTRAINT pk_groups_microgrids                 PRIMARY KEY (group_id, microgrid_id),
+  CONSTRAINT fk_groups_microgrids_groups_01       FOREIGN KEY (group_id) REFERENCES groups(id),
+  CONSTRAINT fk_groups_microgrids_microgrids_02   FOREIGN KEY (microgrid_id) REFERENCES microgrids(id)
+);
+
+# --- !Downs
+
+DROP TABLE IF EXISTS groups_microgrids;
+DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS microgrids;
