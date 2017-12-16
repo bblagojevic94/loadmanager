f2b1965296110e03e82dbbdbfc20011125a6aa05
Bojan Blagojevic <datiglavaradi@gmail.com>
2017-12-16 21:25:42 +0100

    Integrate microgrid DAO layer with controller.
    Update evolution to be able to do cascade delete.

--- app/io/mainflux/loadmanager/controllers/Microgrids.scala
+++ app/io/mainflux/loadmanager/controllers/Microgrids.scala
@@ -2,31 +2,59 @@ package io.mainflux.loadmanager.controllers
 
 import javax.inject.Inject
 
+import io.mainflux.loadmanager.engine.EntityNotFound
+import io.mainflux.loadmanager.hateoas.{MicrogridCollectionResponse, MicrogridRequest, MicrogridResponse}
+import io.mainflux.loadmanager.postgres.MicrogridsDAO
 import play.api.http.HttpErrorHandler
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 final class Microgrids @Inject()(
+    microgridsDAO: MicrogridsDAO,
     cc: ControllerComponents,
     eh: HttpErrorHandler
 )(implicit val ec: ExecutionContext)
     extends ApiEndpoint(cc, eh) {
 
   def create: Action[JsValue] = Action.async(parseJsonAPI) { implicit request =>
-    Future.successful(Created("").as(ContentType))
+    request.body
+      .validate[MicrogridRequest]
+      .fold(
+        errors => createErrorResponse(errors),
+        body => {
+          microgridsDAO
+            .save(body.data.toDomain)
+            .map { mg =>
+              Created(Json.toJson(MicrogridResponse.fromDomain(mg))).as(ContentType)
+            }
+        }
+      )
   }
 
   def retrieveOne(id: Long): Action[AnyContent] = Action.async {
-    Future.successful(Ok("").as(ContentType))
+    microgridsDAO
+      .retrieveOne(id)
+      .map {
+        case Some(microgrid) =>
+          Ok(Json.toJson(MicrogridResponse.fromDomain(microgrid))).as(ContentType)
+        case _ => throw EntityNotFound(s"Microgrid with id $id does not exist.")
+      }
   }
 
   def retrieveAll: Action[AnyContent] = Action.async {
-    Future.successful(Ok("").as(ContentType))
+    microgridsDAO.retrieveAll.map { microgrids =>
+      Ok(Json.toJson(MicrogridCollectionResponse.fromDomain(microgrids))).as(ContentType)
+    }
   }
 
   def remove(id: Long): Action[AnyContent] = Action.async {
-    Future.successful(NoContent)
+    microgridsDAO
+      .remove(id)
+      .flatMap {
+        case 0 => Future.failed(EntityNotFound(s"Microgrid with id $id does not exist."))
+        case _ => Future.successful(NoContent)
+      }
   }
 }
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -24,8 +24,8 @@ CREATE TABLE groups_microgrids (
   microgrid_id   BIGSERIAL NOT NULL,
 
   CONSTRAINT pk_gm                                PRIMARY KEY (group_id, microgrid_id),
-  CONSTRAINT fk_gm_groups                         FOREIGN KEY (group_id) REFERENCES groups(id),
-  CONSTRAINT fk_gm_grids                          FOREIGN KEY (microgrid_id) REFERENCES microgrids(id)
+  CONSTRAINT fk_gm_groups                         FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
+  CONSTRAINT fk_gm_grids                          FOREIGN KEY (microgrid_id) REFERENCES microgrids(id) ON DELETE CASCADE
 );
 
 CREATE TABLE subscribers_groups (
@@ -33,8 +33,8 @@ CREATE TABLE subscribers_groups (
   group_id          BIGSERIAL NOT NULL,
 
   CONSTRAINT pk_sg                                  PRIMARY KEY (subscriber_id, group_id),
-  CONSTRAINT fk_sg_subs                             FOREIGN KEY (subscriber_id) REFERENCES subscribers(id),
-  CONSTRAINT fk_sg_groups                           FOREIGN KEY (group_id) REFERENCES groups(id)
+  CONSTRAINT fk_sg_subs                             FOREIGN KEY (subscriber_id) REFERENCES subscribers(id) ON DELETE CASCADE,
+  CONSTRAINT fk_sg_groups                           FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
 );
 
 # --- !Downs
